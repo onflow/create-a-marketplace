53bdb720a01cd3f5746fa245c86ade78ee2894d7
lealobanov <lea.lobanov@icloud.com>
2023-10-17 21:13:10 +0900

    Debugging dates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const transactionExplanationPath = `${recipe}/explanations/transaction.txt`;
 export const createAMarketplace= {
   slug: recipe,
   title: "Create a Marketplace",
-  createdAt: Date(2022, 9, 14),
+  createdAt: new Date(2022, 9, 14),
   author: "Flow Blockchain",
   playgroundLink:
     "https://play.onflow.org/9f2f0aee-490e-4ffb-8f42-1d1067949dee?type=account&id=84aa1c58-bb11-4d12-aab4-5de3c3d0857e&storage=none",
