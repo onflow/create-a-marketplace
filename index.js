c32d445f1f15f20d57fcae7e7f565c12e20cd977
lealobanov <lea.lobanov@icloud.com>
2023-10-26 00:26:31 +0900

    Create filters and assign difficulty

--- index.js
+++ index.js
@@ -22,5 +22,8 @@ export const createAMarketplace= {
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
   transactionExplanation: transactionExplanationPath,
+  filters: {
+    difficulty: "intermediate"
+  }
 };
 
